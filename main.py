e8dbb489aaf7f251bac49dbbeeca55e2185a8aec
PKHG <pkhgragert@gmail.com>
2021-12-15 17:51:50 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.py
@@ -0,0 +1,13 @@
+def on_button_pressed_a():
+    basic.show_icon(IconNames.HEART)
+    
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+basic.show_leds("""
+    . # . . #
+        . . # # .
+        . . . . .
+        . . . . .
+        . . . . .
+""")
+basic.show_icon(IconNames.HEART)
---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showIcon(IconNames.Heart)
+})
+basic.showLeds(`
+    . # . . #
+        . . # # .
+        . . . . .
+        . . . . .
+        . . . . .
+`)
+basic.showIcon(IconNames.Heart)
